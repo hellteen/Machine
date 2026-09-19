58a639211ed5b0586789f2a5e19e10e05ea69412
Lxsterx <527imartynov@gmail.com>
2026-09-19 15:17:22 +0300

    добавил скрипт для перехода на сцену

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [Tooltip("Название сцены, на которую нужно перейти")]
+    [SerializeField] private string targetSceneName = "Simulation";
+
+    // Метод для вызова по нажатию кнопки
+    public void LoadSimulationScene()
+    {
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
